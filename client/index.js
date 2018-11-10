ed4750c0c8aeea7f48cf0c852c6381771a52c8cb
captDaylight <paul.christophe6@gmail.com>
2018-11-10 14:25:19 -0500

    wrap app in apollo provider

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import Auth from './components/Auth';
 
+const client = new ApolloClient({ uri: 'http://localhost:4000'});
+
 const App = () => (
-  <div>
+  <ApolloProvider client={client}>
     Hi
     <Auth type="SIGNUP" />
-  </div>
+  </ApolloProvider>
 );
 
 render(<App />, document.getElementById('app'));
