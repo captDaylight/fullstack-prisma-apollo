62e664a3964b79fe8e6b68bc1cc49d993b9626bc
captDaylight <paul.christophe6@gmail.com>
2018-11-10 14:07:16 -0500

    base auth

---
+++ client/components/Auth.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Auth extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      isSignup: props.type === 'SIGNUP',
+    };
+  }
+
+  render() {
+    const {
+      email,
+      password,
+      name,
+      isSignup,
+    } = this.state;
+
+    return (
+      <div>
+        <h1>{isSignup ? 'Sign Up' : 'Log In'}</h1>
+
+      </div>
+    );
+  }
+}
+
+Auth.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default Auth;
--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Auth from './components/Auth';
 
 const App = () => (
   <div>
     Hi
+    <Auth type="SIGNUP" />
   </div>
 );
 
