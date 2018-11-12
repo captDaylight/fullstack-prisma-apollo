4400cae28499fb497ddff0a400b9fcb80e98f475
captDaylight <paul.christophe6@gmail.com>
2018-11-12 14:24:58 -0500

    adding in forms for signup and login

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Mutation } from 'react-apollo';
 import { SIGNUP, LOGIN } from '../apollo/mutations';
 
 class Auth extends PureComponent {
@@ -26,6 +27,54 @@ class Auth extends PureComponent {
       <div>
         <h1>{isSignup ? 'Sign Up' : 'Log In'}</h1>
 
+        <Mutation
+          mutation={isSignup ? SIGNUP : LOGIN}
+          variables={isSignup ? { email, name, password } : { email, password }}
+          onCompleted={(res) => console.log('complete', res)}
+        >
+          {
+            (mutation, { error }) => {
+              <form>
+                {
+                  isSignup
+                  && (
+                    <React.Fragment>
+                      <label htmlFor="auth-name">Name</label>
+                      <input
+                        type="name"
+                        name="auth-name"
+                        id="auth-name"
+                        value={name}
+                        onChange={(e) => this.setState({ name: e.target.value })}
+                        required
+                      />
+                    </React.Fragment>
+                  )
+                }
+
+                <label htmlFor="auth-email">Email</label>
+                <input
+                  type="email"
+                  name="auth-email"
+                  id="auth-email"
+                  value={email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                  required
+                />
+
+                <label htmlFor="auth-password">Password</label>
+                <input
+                  type="password"
+                  name="auth-password"
+                  id="auth-password"
+                  value={password}
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                  required
+                />
+              </form>
+            }
+          }
+        </Mutation>
       </div>
     );
   }
