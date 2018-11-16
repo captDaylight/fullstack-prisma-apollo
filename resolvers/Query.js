bb648e82076d54ea936e04a0892d4678bc14b71e
captDaylight <paul.christophe6@gmail.com>
2018-11-16 10:44:54 -0500

    is logged in query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,3 +1,5 @@
+const isLoggedIn = (parent, args, { req }) => typeof req.session.user !== 'undefined';
+
 async function user(parent, args, context, info) {
   return context.db.query.user({
     where: {
@@ -7,5 +9,6 @@ async function user(parent, args, context, info) {
 }
 
 module.exports = {
+  isLoggedIn,
   user,
 };
